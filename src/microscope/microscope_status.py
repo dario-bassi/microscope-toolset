af192468a437c2006b0d6797aaea5a6fa5191591
dario <dario.bassi111@gmail.com>
2026-01-28 17:03:09 +0100

    updated microscoe status

--- src/microscope/microscope_status.py
+++ src/microscope/microscope_status.py
@@ -12,7 +12,7 @@ class MicroscopeStatus:
     def get_current_status(self) -> dict:
 
         code = "print(mmc.getSystemState().dict())"
-        result_status = self.executor.run_code(code)
+        result_status = self.executor._run_code_live(code)
         # with the virtual microscope, maybe we get as key a python object
         # because of this we clean it
         cleaned_string = re.sub(r"<[^:]+:\s+'([^']+)'\s*>", r"'\1'", result_status)
@@ -25,7 +25,7 @@ class MicroscopeStatus:
     def get_properties(self) -> dict:
 
         code ="tmp_list=[(x, mmc.getDeviceSchema(x)) for x in mmc.getLoadedDevices()]\nall_properties = dict(tmp_list)\nprint(all_properties)"
-        result_properties = self.executor.run_code(code)
+        result_properties = self.executor._run_code_live(code)
         # with the virtual microscope, maybe we get as key a python object
         # because of this we clean it
         cleaned_string = re.sub(r"<[^:]+:\s+'([^']+)'\s*>", r"'\1'", result_properties)
@@ -39,7 +39,7 @@ class MicroscopeStatus:
 
         code = "complete_dict={}\nfor x in mmc.getAvailableConfigGroups():\n\ttmp_dict={}\n\tfor y in mmc.getAvailableConfigs(x):\n\t\tconfiguration = mmc.getConfigData(x,y).dict()\n\t\ttmp_dict[y] = configuration\n\tcomplete_dict[x]=tmp_dict\nprint(complete_dict)"
 
-        available_config = self.executor.run_code(code)
+        available_config = self.executor._run_code_live(code)
         # with the virtual microscope, maybe we get as key a python object
         # because of this we clean it
         cleaned_string = re.sub(r"<[^:]+:\s+'([^']+)'\s*>", r"'\1'", available_config)
